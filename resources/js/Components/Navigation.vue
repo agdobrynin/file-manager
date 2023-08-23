265e51f552505f4989391aab96b108fcd5976994
Alexey Dobrynin <ag.dobrynin@gmail.com>
2023-08-23 19:26:58 +0400

    feat: Vue components for base template

---
+++ resources/js/Components/Navigation.vue
@@ -0,0 +1,30 @@
+<template>
+    <nav class="min-w-[200px]">
+        <div class="h-[80px] px-3 flex items-center gap-3">
+            <Link href="/">
+                <ApplicationLogo class="block h-9 w-auto fill-current text-indigo-400"/>
+            </Link>
+            <Link href="/">
+                LaraStorage
+            </Link>
+        </div>
+        <div class="px-3">
+            <div class="px-3">
+                <CreateNewDropdown/>
+            </div>
+            <div class="py-3">
+                <NavLink href="">My files</NavLink>
+                <NavLink href="">Shared with me</NavLink>
+                <NavLink href="">Shared by me</NavLink>
+                <NavLink href="">Trash</NavLink>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<script setup>
+import {Link} from "@inertiajs/vue3";
+import ApplicationLogo from "@/Components/ApplicationLogo.vue";
+import CreateNewDropdown from "@/Components/CreateNewDropdown.vue";
+import NavLink from "@/Components/NavLink.vue";
+</script>
---
+++ resources/js/Components/SearchForm.vue
@@ -0,0 +1,20 @@
+<template>
+    <form class="w-[600px] h-[80px] flex items-center">
+        <TextInput
+            v-model="form.search"
+            type="text"
+            class="block w-full mr-2"
+            placeholder="Serach for files and filders"
+            autocomplete
+        />
+    </form>
+</template>
+
+<script setup>
+import TextInput from "@/Components/TextInput.vue";
+import {useForm} from "@inertiajs/vue3";
+
+const form = useForm({
+    search: ''
+});
+</script>
