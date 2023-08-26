aa450b2daa6b882ff2ce905083a82c0ca4ad1cca
Alexey Dobrynin <ag.dobrynin@gmail.com>
2023-08-26 19:53:22 +0400

    feat: Create root folder for new registered user.

--- app/Models/File.php
+++ app/Models/File.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\Auth;
 use Kalnoy\Nestedset\NodeTrait;
 
 class File extends Model
@@ -12,4 +13,29 @@ class File extends Model
     use HasFactory;
     use NodeTrait;
     use SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'path',
+        'is_folder',
+        'mime',
+        'size',
+    ];
+    protected $casts = [
+        'is_folder' => 'boolean'
+    ];
+
+    public static function boot(): void
+    {
+        parent::boot();
+
+        static::creating(static function (File $model) {
+            $model->created_by = Auth::id();
+            $model->updated_by = Auth::id();
+        });
+
+        static::updating(static function (File $model) {
+            $model->updated_by = Auth::id();
+        });
+    }
 }
