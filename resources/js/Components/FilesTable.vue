e79233e533107cd945f1349f5fc0f94a5ad169d1
Alexey Dobrynin <ag.dobrynin@gmail.com>
2023-09-28 17:32:26 +0400

    feat: New component FilesTable. Paginate for MyTrash component.

---
+++ resources/js/Components/FilesTable.vue
@@ -0,0 +1,198 @@
+<template>
+    <div class="bg-white shadow sm:rounded-lg">
+        <table class="min-w-full">
+            <thead class="bg-gray-100 border-b">
+            <tr>
+                <th class="w-[40px] px-3 text-left">#</th>
+                <th class="w-[40px]">
+                    <Checkbox
+                        v-model="value"
+                        :checked="value"
+                        :disabled="!files.length"
+                        :value="selectAllFilesSymbol"/>
+                </th>
+                <th class="my-files-table-head">Name</th>
+                <th v-if="displayOwner" class="my-files-table-head">Owner</th>
+                <th v-if="displayPath" class="my-files-table-head">Path</th>
+                <th v-if="displayDeletedAt" class="my-files-table-head">Deleted</th>
+                <th v-if="displayLastModified" class="my-files-table-head">Last modified</th>
+                <th class="my-files-table-head">Size</th>
+                <th class="my-files-table-head">Disk</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr v-for="(item, index) of files"
+                :key="item.id"
+                :class="[all || value.includes(item.id) ? '!bg-amber-100 hover:!bg-amber-200': '']"
+                class="cursor-pointer my-files-table-row"
+                @click.stop="clickItem(item)"
+                @dblclick.prevent="$emit('itemDoubleClick', item)"
+            >
+                <td class="px-3 font-light text-sm text-slate-400">
+                    {{ index + 1 }}
+                </td>
+                <td class="w-[40px] text-center">
+                    <Checkbox
+                        v-model="value"
+                        :checked="!!all || value"
+                        :disabled="all"
+                        :value="item.id"/>
+                </td>
+                <td class="my-files-table-cell flex items-center gap-2">
+                    <div>
+                        <FileIcon :mime-type="item.mime" size="30"/>
+                    </div>
+                    <div>{{ item.name }}</div>
+                </td>
+                <td v-if="displayOwner" class="my-files-table-cell">
+                    {{ item.owner }}
+                </td>
+                <td v-if="displayPath" class="my-files-table-cell">
+                    {{ item.path }}
+                </td>
+                <td v-if="displayDeletedAt" class="my-files-table-cell">
+                    {{ item.deletedAt }}
+                </td>
+                <td v-if="displayLastModified" class="my-files-table-cell">
+                    {{ item.updatedAt }}
+                </td>
+                <td class="my-files-table-cell">
+                    {{ item.size ? bytesToSize(item.size) : '' }}
+                </td>
+                <td class="ps-5">
+                    <SvgIcon v-if="!item.isFolder" :path="diskIcon(item)"/>
+                </td>
+            </tr>
+            </tbody>
+        </table>
+        <div v-if="fetchFiles">
+            <div class="p-4 flex items-center gap-2 animate-pulse text-indigo-600">
+                <div>
+                    <FileIcon class="animate-spin" mime-type="text" size="30"/>
+                </div>
+                <div>Please wait. Loading files...</div>
+            </div>
+        </div>
+        <div v-if="files.length === 0" class="py-8 text-center text-sm text-gray-400">
+            There is no data in this folder
+        </div>
+        <div ref="endOfFilesList"></div>
+    </div>
+</template>
+
+<script setup>
+import { bytesToSize } from "@/helpers/helper.js";
+import SvgIcon from "vue3-icon";
+import FileIcon from "@/Components/FileIcon.vue";
+import Checkbox from "@/Components/Checkbox.vue";
+import { mdiCloudOutline, mdiHarddisk } from "@mdi/js";
+import { computed, onMounted, onUpdated, ref } from "vue";
+
+/**
+ * @typedef file
+ * @type {Object}
+ * @property {number} id
+ * @property {string} createdAt
+ * @property {number} createdBy
+ * @property {string} disk
+ * @property {boolean} isFolder
+ * @property {string} mime Mime type
+ * @property {string} name File name
+ * @property {string} owner Name if owner
+ * @property {number} parentId Parent file
+ * @property {string} path Full path
+ * @property {number} size Bytes of file
+ * @property {string} updatedAt
+ * @property {string} updatedBy
+ * @property {string|null} deletedAt
+ */
+/**
+ * @var {Prettify<Readonly<ExtractPropTypes<{
+ *      modelValue: ArrayConstructor|file[],
+ *      fetchFile: boolean,
+ *      selectAllFilesSymbol: string,
+ *      modelValue: string[]|number[]
+ *  }>>>} props
+ */
+const props = defineProps({
+    modelValue: Array,
+    files: Array,
+    fetchFiles: Boolean,
+    displayOwner: {
+        type: Boolean,
+        default: true,
+    },
+    displayLastModified: {
+        type: Boolean,
+        default: true,
+    },
+    displayDeletedAt: {
+        type: Boolean,
+        default: false,
+    },
+    displayPath: {
+        type: Boolean,
+        default: false,
+    },
+    selectAllFilesSymbol: {
+        type: String,
+        default: '*',
+        validator(value) {
+            return value.length > 1;
+        }
+    }
+});
+
+const emit = defineEmits([
+    'update:modelValue',
+    'itemDoubleClick',
+    'canLoad'
+]);
+
+const endOfFilesList = ref(null);
+
+/**
+ * @var {WritableComputedRef<string[]|number[]>} value
+ */
+const value = computed({
+    get: () => props.modelValue,
+    set: (value) => emit('update:modelValue', value),
+});
+
+const all = computed(() => !!value.value.find((v) => v === props.selectAllFilesSymbol));
+
+const diskIcon = (item) => item.disk === 'cloud' ? mdiCloudOutline : mdiHarddisk;
+
+const clickItem = (item) => {
+    if (!all.value) {
+        const index = value.value.indexOf(item.id);
+
+        if (index >= 0) {
+            value.value = value.value.slice(index, 1);
+        } else {
+            value.value.push(item.id);
+        }
+    }
+};
+
+const observer = new IntersectionObserver(
+    async (entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                emit('canLoad');
+            }
+        })
+    },
+    {
+        rootMargin: '-250px 0px 0px 0px'
+    }
+)
+
+onUpdated(() => {
+    observer.observe(endOfFilesList.value);
+})
+
+onMounted(() => {
+    observer.observe(endOfFilesList.value);
+});
+</script>
--- resources/js/Pages/MyTrash.vue
+++ resources/js/Pages/MyTrash.vue
@@ -2,15 +2,73 @@
     <Head title="My files"/>
 
     <AuthenticatedLayout class="relative">
-        <h1 class="text-3xl text-indigo-700">My trashed files here</h1>
+        {{ selectedFileIds }}
+        {{ nextPage }}
+        {{ totalFiles }}
+        <FilesTable
+            v-model="selectedFileIds"
+            :display-deleted-at="true"
+            :display-last-modified="true"
+            :display-owner="false"
+            :display-path="true"
+            :fetch-files="fetchFiles"
+            :files="allFiles"
+            :select-all-files-symbol="SELECTED_ALL_FILES_SYMBOL"
+            class="w-full overflow-auto"
+            @item-double-click="doItemAction"
+            @can-load="doLoadFiles"
+        />
     </AuthenticatedLayout>
 </template>
 
 <script setup>
-import { Head } from "@inertiajs/vue3";
+import { Head, router, usePage } from "@inertiajs/vue3";
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
+import FilesTable from "@/Components/FilesTable.vue";
+import { computed, ref, watchEffect } from "vue";
+
+const SELECTED_ALL_FILES_SYMBOL = 'all';
 
 const props = defineProps({
     files: Object,
 });
+
+const selectedFileIds = ref([]);
+const fetchFiles = ref(false);
+const allFiles = ref(props.files?.data || []);
+const initUrl = usePage().url;
+
+const nextPage = computed(() => props.files?.links?.next || null);
+const totalFiles = computed(() => props.files?.meta?.total || 0);
+
+watchEffect(() => {
+    if (selectedFileIds.value.length > 1
+        && selectedFileIds.value.indexOf(SELECTED_ALL_FILES_SYMBOL) >= 0) {
+        selectedFileIds.value = [ SELECTED_ALL_FILES_SYMBOL ];
+    }
+})
+
+const doLoadFiles = () => {
+    if (nextPage.value && !fetchFiles.value) {
+        router.visit(String(nextPage.value),
+            {
+                preserveState: true,
+                onStart: function () {
+                    fetchFiles.value = true;
+                },
+                onFinish: function () {
+                    fetchFiles.value = false;
+                },
+                onSuccess: ({ props: { files: { data = [] } } }) => {
+                    allFiles.value = [ ...allFiles.value, ...data ];
+                    window.history.replaceState({}, usePage().url, initUrl);
+                },
+            })
+    }
+}
+
+
+const doItemAction = (file) => {
+    console.log(file);
+};
 </script>
