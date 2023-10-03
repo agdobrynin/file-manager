52015de550e9f6bdde138c02c90c7ffef9d0e418
Alexey Dobrynin <ag.dobrynin@gmail.com>
2023-10-03 11:41:20 +0400

    feat: SearchForm has button clear value and v-model for component.

--- resources/js/Components/SearchForm.vue
+++ resources/js/Components/SearchForm.vue
@@ -1,20 +1,45 @@
 <template>
-    <form class="w-full h-[80px] flex items-center">
-        <TextInput
-            v-model="form.search"
-            type="text"
-            class="block w-full mr-2"
-            placeholder="Serach for files and filders"
-            autocomplete="off"
+    <div class="w-full h-[80px] flex items-center relative">
+        <SvgIcon
+            v-if="value.length"
+            :path="mdiClose"
+            class="absolute top-0 right-3 flex h-full items-center cursor-pointer text-gray-600"
+            @click="onClear"
         />
-    </form>
+        <TextInput
+            v-model="value"
+            :placeholder="placeholder"
+            autocomplete="off"
+            class="block w-full mr-2 pr-7"
+            type="text"
+        />
+    </div>
 </template>
 
 <script setup>
 import TextInput from "@/Components/TextInput.vue";
-import {useForm} from "@inertiajs/vue3";
+import { mdiClose } from "@mdi/js";
+import { computed } from "vue";
+import SvgIcon from "vue3-icon";
 
-const form = useForm({
-    search: ''
+const props = defineProps({
+    modelValue: String,
+    placeholder: String,
 });
+
+const emit = defineEmits([ 'update:modelValue', 'onClear' ]);
+
+const value = computed({
+    get() {
+        return props.modelValue;
+    },
+    set(value) {
+        emit('update:modelValue', value);
+    }
+});
+
+const onClear = () => {
+    value.value = '';
+    emit('onClear');
+};
 </script>
--- resources/js/helpers/helper.js
+++ resources/js/helpers/helper.js
@@ -5,9 +5,9 @@
  * @return {string}
  */
 export function bytesToSize(bytes, factorDigits = 2) {
-    const units = ['byte', 'kB', 'Mb', 'Gb', 'Tb'];
-
+    const units = [ 'byte', 'kB', 'Mb', 'Gb', 'Tb' ];
+    
     const unitIndex = Math.max(0, Math.min(Math.floor(Math.log(bytes) / Math.log(1024)), units.length - 1));
-
+    
     return `${(bytes / (1024 ** unitIndex)).toFixed(factorDigits)} ${units[unitIndex]}`;
 }
