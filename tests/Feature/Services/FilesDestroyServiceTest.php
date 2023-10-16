fa59397129bc1d70bd2e7c268868da73cb711882
Alexey Dobrynin <ag.dobrynin@gmail.com>
2023-10-16 16:16:22 +0400

    test: Test for FilesDestroyServiceTest.

---
+++ tests/Feature/Services/FilesDestroyServiceTest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Tests\Feature\Services;
+
+use App\Dto\DestroyFileFromStorageDto;
+use App\Models\File;
+use App\Models\User;
+use App\Services\FilesDestroyService;
+use Illuminate\Database\Eloquent\Factories\Sequence;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Str;
+use Tests\TestCase;
+
+class FilesDestroyServiceTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_files_destroy_collection_not_contain_file_models(): void
+    {
+        $collection = User::factory(10)->create();
+        $collection->push('abc', 'aaa', 'ccc');
+        $service = new FilesDestroyService();
+
+        $this->assertCount(0, $service->destroy($collection));
+    }
+
+    public function test_files_destroy_success(): void
+    {
+        $user = User::factory()->create();
+        Auth::shouldReceive('id')->andReturn($user->id);
+
+        /** @var File $root */
+        $root = File::factory()->isFolder()->create(['name' => $user->email]);
+        File::factory(3)
+            ->state(new Sequence(fn() => ['storage_path' => '/files/' . $root->id . '/' . Str::uuid()]))
+            ->afterCreating(fn(File $file) => $file->appendToNode($root)->save())
+            ->create();
+        $subFolder = File::factory()->isFolder()
+            ->afterCreating(fn(File $file) => $file->appendToNode($root)->save())
+            ->create();
+        File::factory(2)
+            ->state(new Sequence(fn() => ['storage_path' => '/files/' . $subFolder->id . '/' . Str::uuid()]))
+            ->afterCreating(fn(File $file) => $file->appendToNode($subFolder)->save())
+            ->create();
+
+        $service = new FilesDestroyService();
+        $res = $service->destroy($root->children);
+
+        $this->assertCount(5, $res);
+        $this->assertContainsOnlyInstancesOf(DestroyFileFromStorageDto::class, $res);
+    }
+}
