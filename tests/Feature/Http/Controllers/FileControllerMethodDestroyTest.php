bd391858b7a93638bde91a059ce0b289b9c145b7
Alexey Dobrynin <ag.dobrynin@gmail.com>
2023-10-20 23:23:05 +0400

    test: Test for FileController::destroy.

---
+++ tests/Feature/Http/Controllers/FileControllerMethodDestroyTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Tests\Feature\Http\Controllers;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class FileControllerMethodDestroyTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_example(): void
+    {
+    }
+}
