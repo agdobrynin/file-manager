bf67c1a8d66009d2a3c37b547d7723a74f9ead09
Alexey Dobrynin <ag.dobrynin@gmail.com>
2023-10-20 16:07:46 +0400

    test: Test for FileController::upload.

---
+++ tests/Feature/Http/Controllers/FileControllerMethodUpload.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Tests\Feature\Http\Controllers;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class FileControllerMethodUpload extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_example(): void
+    {
+    }
+}
